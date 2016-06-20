5d8b0b02089ccf1999dc33200cf4d8c5bd490e22
testsz <testsz@users.noreply.github.com>
2016-06-20 04:23:04 -0700

    Update HelloWorld.java
    test

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Bueno, Mars - Attempt 2!");
+        System.out.println("Bueno, Mars - Attempt 3!");
     }
 
 }
